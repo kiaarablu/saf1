c5650dad56eaf3879b7c91d331ec1900f1822d4a
kiaarablu <109739665+kiaarablu@users.noreply.github.com>
2022-07-25 14:02:40 +0430

    Add files via upload

---
+++ Saf_1/Saf_1/Program.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Saf_1
+{
+     
+    internal class Program
+    {
+        int[] safarray;
+        int front=0;
+        int rear=0;
+        int arraysize;
+
+        static void Main(string[] args)
+        {
+        }
+        public bool isfull()
+        {
+            if (safarray.Length >= rear)
+            {
+                Console.WriteLine("array is full");
+                return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        public bool isempty()
+        {
+            if (safarray.Length <= front)
+            {
+                Console.WriteLine("array is empty");
+               return false;
+            }
+            else
+            {
+                return true;
+            }
+
+        }
+        public void Enqueue(int Number)
+        {
+            if (!isfull())
+            {
+                 safarray[rear] = Number;
+                 rear++;
+            }
+            else
+            {
+                Console.WriteLine("array is full");
+            }
+                
+        }
+        public int Dequeue()
+        {
+            if (!isempty())
+            {
+                int res = safarray[front];
+                for (int i = 0; i < safarray.Length; i++)
+                {
+                    safarray[i] = safarray[i + 1];
+                }
+                rear--;
+                return res;
+            }
+           return -1;
+        }
+        public void showarray()
+        {
+            if (!isempty())
+            {
+                for (int i =front;i<rear ; i++)
+                {
+                    Console.WriteLine(safarray[i]);
+                }
+            }
+
+        }
+    }
+}
